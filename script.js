8f9750fe15266bd9b49de6e2d8ae3cbb9368b947
Euwamahoro <133766701+Euwamahoro@users.noreply.github.com>
2023-12-11 22:34:43 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,110 +1,58 @@
-const DEFAULT_COLOR = '#333333'
-const DEFAULT_MODE = 'color'
-const DEFAULT_SIZE = 16
+let availableKeyWords = [
+    'live score',
+    'premier league',
+    'serie a',
+    'la liga',
+    'bundesliga',
+    'transfer news',
+    'ucl',
+    'europa league',
+    'top scorers',
+    'lionel messi',
+    'cristian ronaldo',
+    'haaland',
+    'mbappe',
+    'bellingham',
+    'man u',
+    'barca',
+    'man city',
+    'chelsea',
+    'liverpool',
+    'psg',
+    'guardiola',
+    'kloop',
+    'ten haag'
 
-let currentColor = DEFAULT_COLOR
-let currentMode = DEFAULT_MODE
-let currentSize = DEFAULT_SIZE
+];
 
-function setCurrentColor(newColor) {
-  currentColor = newColor
+const resultsBox = document.querySelector(".results");
+const inputBox = document.getElementById("search-box");
+
+inputBox.onkeyup = function() {
+    let result = [];
+    let input = inputBox.value;
+    if (input.length) {
+        result = availableKeyWords.filter((keyword)=>{
+            return keyword.toLowerCase().includes(input.toLowerCase());
+        });
+        console.log(result);       
+    }
+    display(result);
+
+    if(!result.length) {
+        resultsBox.innerHTML = '';
+    }
 }
 
-function setCurrentMode(newMode) {
-  activateButton(newMode)
-  currentMode = newMode
+function display(result){
+    const content = result.map((list)=>{
+        return "<li onclick=selectInput(this)>" + list + "</li>";
+    });
+
+    resultsBox.innerHTML = "<ul>" + content.join('') + "</ul>";
 }
 
-function setCurrentSize(newSize) {
-  currentSize = newSize
-}
-
-const colorPicker = document.getElementById('colorPicker')
-const colorBtn = document.getElementById('colorBtn')
-const rainbowBtn = document.getElementById('rainbowBtn')
-const eraserBtn = document.getElementById('eraserBtn')
-const clearBtn = document.getElementById('clearBtn')
-const sizeValue = document.getElementById('sizeValue')
-const sizeSlider = document.getElementById('sizeSlider')
-const grid = document.getElementById('grid')
-
-colorPicker.oninput = (e) => setCurrentColor(e.target.value)
-colorBtn.onclick = () => setCurrentMode('color')
-rainbowBtn.onclick = () => setCurrentMode('rainbow')
-eraserBtn.onclick = () => setCurrentMode('eraser')
-clearBtn.onclick = () => reloadGrid()
-sizeSlider.onmousemove = (e) => updateSizeValue(e.target.value)
-sizeSlider.onchange = (e) => changeSize(e.target.value)
-
-let mouseDown = false
-document.body.onmousedown = () => (mouseDown = true)
-document.body.onmouseup = () => (mouseDown = false)
-
-function changeSize(value) {
-  setCurrentSize(value)
-  updateSizeValue(value)
-  reloadGrid()
-}
-
-function updateSizeValue(value) {
-  sizeValue.innerHTML = `${value} x ${value}`
-}
-
-function reloadGrid() {
-  clearGrid()
-  setupGrid(currentSize)
-}
-
-function clearGrid() {
-  grid.innerHTML = ''
-}
-
-function setupGrid(size) {
-  grid.style.gridTemplateColumns = `repeat(${size}, 1fr)`
-  grid.style.gridTemplateRows = `repeat(${size}, 1fr)`
-
-  for (let i = 0; i < size * size; i++) {
-    const gridElement = document.createElement('div')
-    gridElement.classList.add('grid-element')
-    gridElement.addEventListener('mouseover', changeColor)
-    gridElement.addEventListener('mousedown', changeColor)
-    grid.appendChild(gridElement)
-  }
-}
-
-function changeColor(e) {
-  if (e.type === 'mouseover' && !mouseDown) return
-  if (currentMode === 'rainbow') {
-    const randomR = Math.floor(Math.random() * 256)
-    const randomG = Math.floor(Math.random() * 256)
-    const randomB = Math.floor(Math.random() * 256)
-    e.target.style.backgroundColor = `rgb(${randomR}, ${randomG}, ${randomB})`
-  } else if (currentMode === 'color') {
-    e.target.style.backgroundColor = currentColor
-  } else if (currentMode === 'eraser') {
-    e.target.style.backgroundColor = '#fefefe'
-  }
-}
-
-function activateButton(newMode) {
-  if (currentMode === 'rainbow') {
-    rainbowBtn.classList.remove('active')
-  } else if (currentMode === 'color') {
-    colorBtn.classList.remove('active')
-  } else if (currentMode === 'eraser') {
-    eraserBtn.classList.remove('active')
-  }
-
-  if (newMode === 'rainbow') {
-    rainbowBtn.classList.add('active')
-  } else if (newMode === 'color') {
-    colorBtn.classList.add('active')
-  } else if (newMode === 'eraser') {
-    eraserBtn.classList.add('active')
-  }
-}
-
-window.onload = () => {
-  setupGrid(DEFAULT_SIZE)
-  activateButton(DEFAULT_MODE)
+function selectInput(list) {
+    inputBox.value = list.innerHTML;
+    resultsBox.innerHTML = '';
 }
